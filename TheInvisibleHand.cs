6cd4ca889747d2fc9d0aa6edbd89bbb0781bec30
kf4btg <Kf4btg@users.noreply.github.com>
2016-03-25 17:21:56 -0400

    Initialize Project

---
+++ TheInvisibleHand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Terraria.ModLoader;
+
+namespace InvisibleHand
+{
+	public class IHBase : Mod
+	{
+		public override void SetModInfo(out string name, ref ModProperties properties)
+		{
+			name = "TheInvisibleHand";
+			properties.Autoload = true;
+			properties.AutoloadGores = true;
+			properties.AutoloadSounds = true;
+		}
+	}
+}
