c9204dba529b1916575f0c7d48ee172ed1893770
kf4btg <Kf4btg@users.noreply.github.com>
2016-04-07 01:01:54 -0400

    Less dynamic, more generic, don't know if works

--- Utils/ModOption.cs
+++ Utils/ModOption.cs
@@ -24,41 +24,94 @@ namespace InvisibleHand.Utils
         private static IDictionary<string, IDictionary<string, dynamic>> OptionsByMod = new Dictionary<string, IDictionary<string, dynamic>>();
 
         /// Called as extension method on the Mod class; the type of the option is inferred dynamically from the type of `default_value`. If this type is one of string, bool, int, float, or Keys, an appropriate subclass ModOption will be instantiated. Otherwise, a new generic ModOption type will be generated and used.
-        public static void RegisterOption(this Mod mod, string option_name, dynamic default_value)
+        public static ModOption<T> RegisterOption<T>(this Mod mod, string option_name, T default_value)
         {
-
-            // if (!trackedMods.ContainsKey(mod.Name))
-            //     trackedMods.Add(mod.Name, mod);
-
-            // special cases
-            dynamic option;
-            if (default_value is string)
-                option = new StringOption(mod, option_name, default_value);
-            else if (default_value is bool)
-                option = new BoolOption(mod, option_name, default_value);
-            else if (default_value is int)
-                option = new IntOption(mod, option_name, default_value);
-            else if (default_value is float)
-                option = new FloatOption(mod, option_name, default_value);
-            else if (default_value is Keys)
-                option = new KeyOption(mod, option_name, default_value);
-            else
-            {
-                // create a ModOption type based on the default value
-                Type t = default_value.GetType();
-                Type constructedOptType = typeof(ModOption<>).MakeGenericType(new Type[] { t });
-
-                option = constructedOptType.GetConstructor(new Type[]{
-                    typeof(Mod),
-                    typeof(string),
-                    t}).Invoke(new object[]{mod, option_name, default_value});
-            }
-
+            // create a ModOption type based on the default value
+            ModOption<T> option = new ModOption<T>(mod, option_name, default_value);
             _addOption(mod.Name, option);
+            return option;
+        }
+        public static ModOption<T> RegisterOption<T>(this Mod mod, string option_name, T default_value, Action<string, T> callback, bool notify = true)
+        {
+            var option = RegisterOption(mod, option_name, default_value);
+            option.RegisterCallback(callback, notify);
+            return option;
         }
 
-        /// register a pre-constructed mod option
-        private static void _addOption<U, T>(string mod_name, U mod_option) where U : ModOption<T>
+        //--- Special Cases ---//
+
+        // string
+        public static StringOption RegisterOption(this Mod mod, string option_name, string default_value)
+        {
+            var option = new StringOption(mod, option_name, default_value);
+            _addOption(mod.Name, option);
+            return option;
+        }
+        public static StringOption RegisterOption(this Mod mod, string option_name, string default_value, Action<string, string> callback, bool notify = true)
+        {
+            var option = RegisterOption(mod, option_name, default_value);
+            option.RegisterCallback(callback, notify);
+            return option;
+        }
+
+        // bool
+        public static BoolOption RegisterOption(this Mod mod, string option_name, bool default_value)
+        {
+            var option = new BoolOption(mod, option_name, default_value);
+            _addOption(mod.Name, option);
+            return option;
+        }
+        public static BoolOption RegisterOption(this Mod mod, string option_name, bool default_value, Action<string, bool> callback, bool notify = true)
+        {
+            var option = RegisterOption(mod, option_name, default_value);
+            option.RegisterCallback(callback, notify);
+            return option;
+        }
+
+        // int
+        public static IntOption RegisterOption(this Mod mod, string option_name, int default_value)
+        {
+            var option = new IntOption(mod, option_name, default_value);
+            _addOption(mod.Name, option);
+            return option;
+        }
+        public static IntOption RegisterOption(this Mod mod, string option_name, int default_value, Action<string, int> callback, bool notify = true)
+        {
+            var option = RegisterOption(mod, option_name, default_value);
+            option.RegisterCallback(callback, notify);
+            return option;
+        }
+
+        // float
+        public static FloatOption RegisterOption(this Mod mod, string option_name, float default_value)
+        {
+            var option = new FloatOption(mod, option_name, default_value);
+            _addOption(mod.Name, option);
+            return option;
+        }
+        public static FloatOption RegisterOption(this Mod mod, string option_name, float default_value, Action<string, float> callback, bool notify = true)
+        {
+            var option = RegisterOption(mod, option_name, default_value);
+            option.RegisterCallback(callback, notify);
+            return option;
+        }
+
+        // Keys
+        public static KeyOption RegisterOption(this Mod mod, string option_name, Keys default_value)
+        {
+            var option = new KeyOption(mod, option_name, default_value);
+            _addOption(mod.Name, option);
+            return option;
+        }
+        public static KeyOption RegisterOption(this Mod mod, string option_name, Keys default_value, Action<string, Keys> callback, bool notify = true)
+        {
+            var option = RegisterOption(mod, option_name, default_value);
+            option.RegisterCallback(callback, notify);
+            return option;
+        }
+
+        /// register a pre-constructed  mod option
+        private static ModOption<T> _addOption<T>(string mod_name, ModOption<T> mod_option)
         {
             if (!OptionsByMod.ContainsKey(mod_name))
                 OptionsByMod.Add(mod_name, new Dictionary<string, dynamic>());
@@ -66,18 +119,25 @@ namespace InvisibleHand.Utils
             try
             {
                 OptionsByMod[mod_name].Add(mod_option.name, mod_option);
+                return mod_option;
             }
             catch (ArgumentException)
             {
                 // don't crash on duplicate keys; but do write an error to the log
                 ErrorLogger.Log($"An Option with the name {mod_option.name} is already registered for mod {mod_name}.");
             }
+            return null;
         }
 
-        /// register a pre-constructed mod option for this mod
-        public static void AddOption<U, T>(this Mod mod, U mod_option) where U : ModOption<T>
+        /// register a pre-constructed, non-standard (i.e. subclassed) mod option for this mod
+        public static void AddOption<T>(this Mod mod, ModOption<T> mod_option)
         {
-            _addOption<U, T>(mod.Name, mod_option);
+            _addOption<T>(mod.Name, mod_option);
+        }
+        public static void AddOption<T>(this Mod mod, ModOption<T> mod_option, Action<string, T> callback, bool notify = true)
+        {
+            _addOption<T>(mod.Name, mod_option);
+            mod_option.RegisterCallback(callback, notify);
         }
 
         /// request the option value of the type T for the option named `option_name` in this mod
@@ -90,15 +150,24 @@ namespace InvisibleHand.Utils
             return OptionsByMod[mod.Name][option_name].Value;
         }
 
-        public static void UpdateOption<T>(this Mod mod, string option_name, T new_value)
+        /// return the actual ModOption object with the given option_name;
+        public static ModOption<T> GetOption<T>(this Mod mod, string option_name)
+        {
+            return OptionsByMod[mod.Name][option_name];
+        }
+
+        /// Change the value of the stored option; make certain that the type of new_value matches the Type of the ModOption
+        /// If a callback has been registered with this option and notify==true, that callback will be invoked with the name of the option
+        /// and the new_value.
+        public static void UpdateOption(this Mod mod, string option_name, dynamic new_value)
         {
             OptionsByMod[mod.Name][option_name].Value = new_value;
         }
 
-        // public static void RegisterCallback(this Mod mod, string option_name, Action<string)
-        // {
-        //
-        // }
+        public static void RegisterCallback<T>(this Mod mod, string option_name, Action<string, T> callback, bool notify=true)
+        {
+            OptionsByMod[mod.Name][option_name].RegisterCallback(callback, notify);
+        }
     }
 
     /// T is the type of the option's value, usually a simple type such as int, bool, string, etc.
@@ -198,7 +267,7 @@ namespace InvisibleHand.Utils
 
     public class BoolOption : ModOption<bool>
     {
-        public BoolOption(Mod mod, string name, bool value = false) : base(mod, name, value) { }
+        public BoolOption(Mod mod, string name, bool? value = false) : base(mod, name, (bool)value) { }
 
         public static implicit operator bool(BoolOption o)
         {
@@ -208,7 +277,7 @@ namespace InvisibleHand.Utils
 
     public class FloatOption : ModOption<float>
     {
-        public FloatOption(Mod mod, string name, float value = default(float)) : base(mod, name, value) { }
+        public FloatOption(Mod mod, string name, float? value = default(float)) : base(mod, name, (float)value) { }
 
         public static implicit operator float(FloatOption o)
         {
@@ -218,7 +287,7 @@ namespace InvisibleHand.Utils
 
     public class IntOption : ModOption<int>
     {
-        public IntOption(Mod mod, string name, int value = 0) : base(mod, name, value) { }
+        public IntOption(Mod mod, string name, int? value = 0) : base(mod, name, (int)value) { }
 
         public static implicit operator int(IntOption o)
         {
@@ -228,7 +297,7 @@ namespace InvisibleHand.Utils
 
     public class KeyOption : ModOption<Keys>
     {
-        public KeyOption(Mod mod, string name, Keys value = Keys.None) : base(mod, name, value) {}
+        public KeyOption(Mod mod, string name, Keys? value = Keys.None) : base(mod, name, (Keys)value) {}
 
         public static implicit operator Keys(KeyOption o)
         {
