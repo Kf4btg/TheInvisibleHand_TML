0435abad7e8f1e78c1839b033a7d9f51cda83b74
kf4btg <Kf4btg@users.noreply.github.com>
2016-04-20 01:10:40 -0400

    create set of alchemy-ingredient items by examining recipes

--- Items/Sets.cs
+++ Items/Sets.cs
@@ -2,6 +2,7 @@ using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
 using System.Collections.Generic;
+using System.Linq;
 
 
 namespace InvisibleHand.Items
@@ -70,6 +71,8 @@ namespace InvisibleHand.Items
 
         public static readonly Set CraftingStations;
 
+        public static readonly Set AlchemyIngredients;
+
         static TileSets()
         {
             Furniture = new Set("Furniture", TileID.Sets.RoomNeeds.CountsAsDoor,
@@ -79,10 +82,17 @@ namespace InvisibleHand.Items
 
             // this might be a bad idea...let's see!
             CraftingStations = new Set("CraftingStations");
+            AlchemyIngredients = new Set("AlchemyIngredients");
+
+            int tileID;
             foreach (var r in Main.recipe)
             {
-                foreach (var tileID in r.requiredTile)
+                // foreach (var tileID in r.requiredTile)
+
+                // save a bit of gc-pressure
+                for (int i = 0; i < Recipe.maxRequirements; i++)
                 {
+                    tileID = r.requiredTile[i];
                     // the requiredTile array is initialized from 0 on up;
                     // if we found -1 (the default value for the array entries),
                     // then there are no more required tiles for this recipe;
@@ -92,8 +102,17 @@ namespace InvisibleHand.Items
                     if (tileID == -1) break;
                     CraftingStations.Add(tileID);
                 }
+
+                if (r.alchemy)
+                {
+                    // add the item-ids for the the ingredients;
+                    // this isn't technically a 'tile-set', then,
+                    // but I'd rather not go through the Recipes twice
+                    AlchemyIngredients.Extend(r.requiredItem.TakeWhile(i => i.type != 0).Select(i => i.type));
+                }
             }
             CraftingStations.Trim();
+            AlchemyIngredients.Trim();
         }
 
         /// return a set by name
@@ -108,6 +127,8 @@ namespace InvisibleHand.Items
                 case "Crafting":
                 case "CraftingStations":
                     return CraftingStations;
+                case "AlchemyIngredients":
+                    return AlchemyIngredients;
                 default:
                     ErrorLogger.Log($"No such set: '{set_name}'");
                     break;
